ac17f03017843b602999f6f0f9e0cefbf95653a8
sachinsingh <sachinseengh@gmail.com>
2024-01-18 10:40:21 +0545

    Design completed for voterlogin page

--- E-Vote/src/EVote/Main.java
+++ E-Vote/src/EVote/Main.java
@@ -1,20 +1,60 @@
 package EVote;
 	
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
+	 private double x =0;
+	 private double y=0;
 	@Override
 	public void start(Stage primaryStage) {
 		try {
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
+			
+			
+			root.setOnMousePressed(new EventHandler<MouseEvent>(){
+		           @Override
+		           public void handle(MouseEvent event) {
+		               x= event.getSceneX();
+		               y= event.getSceneY();
+		           }
+
+		         });
+
+		         root.setOnMouseDragged(new EventHandler<MouseEvent>(){
+		           @Override
+		           public void handle(MouseEvent event) {
+		              primaryStage.setX(event.getScreenX()-x);
+		              primaryStage.setY(event.getScreenY()-y);
+		              
+		              primaryStage.setOpacity(0.9);
+		           }
+
+		         });
+		         
+		         root.setOnMouseReleased(new EventHandler<MouseEvent>(){
+		           @Override
+		           public void handle(MouseEvent event) {
+		              primaryStage.setOpacity(1);
+		           }
+
+		         });
+			
+			
+			
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.initStyle(StageStyle.TRANSPARENT);
+			primaryStage.setFullScreen(true);
+			
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
--- E-Vote/src/EVote/MainController.java
+++ E-Vote/src/EVote/MainController.java
@@ -1,5 +1,54 @@
 package EVote;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+
+public class MainController implements Initializable {
+
+    @FXML
+    private Label e_txt;
+    
+    @FXML
+    private Label vote_txt;
+    
+    @FXML
+    private Button minimize_btn;
+    
+
+    @FXML
+    private Button close_btn;
+    
+
+    @FXML
+    private Button fullscreen;
+    
+    public void minimize(){
+        Stage stage = (Stage)minimize_btn.getScene().getWindow();
+        stage.setIconified(true);
+    }
+    
+    public void togglefullscreen() {
+    	Stage stage = (Stage)fullscreen.getScene().getWindow();
+    	stage.setFullScreen(!stage.isFullScreen());
+    }
+    
+    
+    public void close() {
+    	System.exit(0);
+    }
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		
+		
+	}
 	
 }
