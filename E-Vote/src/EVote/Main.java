b1c1916ea5bd0a862c531e9aafadf7b16f9bba3f
sachinsingh <sachinseengh@gmail.com>
2024-01-19 02:13:49 +0545

    Voter Dashboard Created

--- E-Vote/src/EVote/Main.java
+++ E-Vote/src/EVote/Main.java
@@ -50,7 +50,7 @@ public class Main extends Application {
 			
 			
 			Scene scene = new Scene(root);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			scene.getStylesheets().add(getClass().getResource("Main.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.initStyle(StageStyle.TRANSPARENT);
 			
